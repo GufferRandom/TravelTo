0d00ed824497dad2ed2104ceea51aa8ef959c4ad
Dato Rogava <datorogavaa@gmail.com>
2024-08-06 05:14:39 +0400

    migracia update users

---
+++ Models/User.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace TravelTo.Models
+{
+    public class User : IdentityUser
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public string EmailConfirmed { get; set; } = string.Empty;
+        [Required]
+        public string PasswordConfirmed { get; set; }
+    }
+}
---
+++ Users/UsersDbContext.cs
@@ -0,0 +1,15 @@
+﻿
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using TravelTo.Models;
+
+namespace TravelTo.Users
+{
+    public class UsersDbContext : IdentityDbContext<IdentityUser>
+    {
+        public UsersDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
