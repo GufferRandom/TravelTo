0abab5f01a818e1b07b33849aaef50a2d9c024f7
GufferRandom <guffera2005@gmail.com>
2024-08-06 20:36:59 +0400

    euh kvaxobebi

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel;
 using System.Diagnostics;
 using TravelTo.Data;
 using TravelTo.Dto;
@@ -73,6 +74,43 @@ namespace TravelTo.Controllers
             var get_turi=_context.Turebi.Where(x=>x.id==id).FirstOrDefault();
             return View(get_turi);
         }
+        [HttpGet,DisplayName("Edit")]
+        public IActionResult Edit(int id) {
+
+            var get_turi2 = _context.Turebi.Where(x => x.id == id).FirstOrDefault();
+           ViewData["img_name"] = get_turi2.image_name;
+           var get_turi_dto= new TurebiDto() { Name=get_turi2.Name,Description=get_turi2.Description,Price=get_turi2.Price };
+
+            return View(get_turi_dto);
+        }
+        [HttpPost]
+        public IActionResult Edit(int id,TurebiDto turebi) {
+            var get_turi = _context.Turebi.Where(x => x.id == id).FirstOrDefault();
+            string wwwrootpath = webHostEnvironment.WebRootPath;
+            string filename = Guid.NewGuid().ToString() + Path.GetExtension(turebi.image.FileName);
+            string productPath = Path.Combine(wwwrootpath, "turebi");
+            if (!Directory.Exists(productPath))
+            {
+                Directory.CreateDirectory(productPath);
+            }
+            using (var fileStream = new FileStream(Path.Combine(productPath, filename), FileMode.Create))
+            {
+                turebi.image.CopyTo(fileStream);
+            }
+            get_turi.Name = turebi.Name;
+            get_turi.image_name =filename;
+            get_turi.Price= turebi.Price;
+            get_turi.Description = turebi.Description;
+            if(ModelState.IsValid)
+            {
+                _context.Update(get_turi);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+                return View();
+
+        }
+
     }
-    }
+}
 
