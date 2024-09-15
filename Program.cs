37742455d7b1e73b33ac7892352c0a3daa33b190
GufferRandom <guffera2005@gmail.com>
2024-09-15 21:22:20 +0400

    vqeeeni

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ using TravelTo.Models;
 
 internal class Program
 {
-    private static void Main(string[] args)
+    private static async Task Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
         var configuration = builder.Configuration;
@@ -27,8 +27,8 @@ internal class Program
         //builder.Services.AddDbContext<UsersDbContext>(options => options.UseSqlServer(
         //    builder.Configuration.GetConnectionString("UserCon")
         //    ));
-        
-        builder.Services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = false).AddEntityFrameworkStores<ApplicationDataContext>();
+
+        builder.Services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = false).AddRoles<IdentityRole>().AddEntityFrameworkStores<ApplicationDataContext>();
         builder.Services.AddRazorPages();
         var app = builder.Build();
 
@@ -39,33 +39,46 @@ internal class Program
             // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
             app.UseHsts();
         }
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
 
+        app.UseRouting();
+        app.UseAuthentication();
+        app.UseAuthorization();
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
+        app.MapRazorPages();
 
-using (var scope = app.Services.CreateScope())
-{
-    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
-    var roles = new[] { "Admin", "Manager", "User" };
-    foreach (var role in roles)
-    {
-        if (!await roleManager.RoleExistsAsync(role))
-        {
-            await roleManager.CreateAsync(new IdentityRole(role));
-        }
-    }
-}
         using (var scope = app.Services.CreateScope())
         {
-            var UserManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+            var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var roles = new[] { "Admin", "Manager", "User" };
+            foreach (var role in roles)
+            {
+                if (!await roleManager.RoleExistsAsync(role))
+                {
+                    await roleManager.CreateAsync(new IdentityRole(role));
+                }
+            }
+        }
+        using (var scope = app.Services.CreateScope())
+        {
+            var UserManager = scope.ServiceProvider.GetRequiredService<UserManager<User>>();
             string email = "admin@admin.com";
             string password = "Admin1111.";
             if (await UserManager.FindByEmailAsync(email) == null)
-                {
-                var user = new IdentityUser();
+            {
+                var user = new User();
                 user.UserName = email;
                 user.Email = email;
-                await  UserManager.CreateAsync(user, password);
+                await UserManager.CreateAsync(user, password);
                 await UserManager.AddToRoleAsync(user, "Admin");
-                }
+            }
 
 
         }
+        app.Run();
+    }
+
+}
