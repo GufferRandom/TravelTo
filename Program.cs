1349216ac4f01c8f45ba73da16ad482791b3c4c8
GufferRandom <guffera2005@gmail.com>
2024-10-15 11:44:00 +0400

    imedia imushavebs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-using System;
+
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
@@ -7,103 +7,97 @@ using TravelTo.Models;
 
 internal class Program
 {
-    private static void Main(string[] args)
+    private static async Task Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
         var configuration = builder.Configuration;
+        //builder.Services.AddAuthentication().AddGoogle(googleOptions => 
+        //{
+        //    googleOptions.ClientId = configuration["Authentication:Google:ClientId"];
+        //    googleOptions.ClientSecret = configuration["Authentication:Google:ClientSecret"];
 
+        //});
+        // Add services to the container.
+        var useSqlite = builder.Configuration.GetConnectionString("UseSqlite"); 
         builder.Services.AddControllersWithViews();
         builder.Services.AddRazorPages();
-
-        bool useInMemoryDatabase = string.IsNullOrEmpty(builder.Configuration.GetConnectionString("UserIsME"));
-        if (useInMemoryDatabase)
+        builder.Services.AddDbContext<ApplicationDataContext>(options =>
         {
-            builder.Services.AddDbContext<ApplicationDataContext>(options =>
-                options.UseInMemoryDatabase("InMemoryDatabase"));
-        }
-        else
-        {
-            builder.Services.AddDbContext<ApplicationDataContext>(options =>
-                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-        }
-
-        builder.Services.AddDistributedMemoryCache();
-        builder.Services.AddSession(options =>
-        {
-            options.IdleTimeout = TimeSpan.FromSeconds(10);
-            options.Cookie.HttpOnly = true;
-            options.Cookie.IsEssential = true;
+            if (useSqlite=="true")
+            {
+                options.UseSqlite("Data Source=app.db");
+            }
+            else
+            {
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            }
         });
+   
+        
+       
+		builder.Services.AddDistributedMemoryCache();
 
-        builder.Services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = false)
-            .AddRoles<IdentityRole>()
-            .AddEntityFrameworkStores<ApplicationDataContext>();
-
+		builder.Services.AddSession(options =>
+		{
+			options.IdleTimeout = TimeSpan.FromSeconds(10);
+			options.Cookie.HttpOnly = true;
+			options.Cookie.IsEssential = true;
+		});
+		//builder.Services.AddDbContext<UsersDbContext>(options => options.UseSqlServer(
+		//    builder.Configuration.GetConnectionString("UserCon")
+		//    ));
+        
+		builder.Services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = false).AddRoles<IdentityRole>().AddEntityFrameworkStores<ApplicationDataContext>();
+        builder.Services.AddRazorPages();
         var app = builder.Build();
 
-        using (var scope = app.Services.CreateScope())
-        {
-            var services = scope.ServiceProvider;
-            try
-            {
-                if (useInMemoryDatabase)
-                {
-                    SeedingData.Initialize(services);
-                }
-                CreateRolesAndAdminUser(services).GetAwaiter().GetResult();
-            }
-            catch (Exception ex)
-            {
-                var logger = services.GetRequiredService<ILogger<Program>>();
-                logger.LogError(ex, "An error occurred seeding the DB or creating roles.");
-            }
-        }
-
+        // Configure the HTTP request pipeline.
         if (!app.Environment.IsDevelopment())
         {
             app.UseExceptionHandler("/Home/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
             app.UseHsts();
         }
-
         app.UseHttpsRedirection();
         app.UseStaticFiles();
+
         app.UseRouting();
         app.UseAuthentication();
         app.UseAuthorization();
-        app.UseSession();
+		app.UseSession();
 
-        app.MapControllerRoute(
+		app.MapControllerRoute(
             name: "default",
             pattern: "{controller=Home}/{action=Index}/{id?}");
         app.MapRazorPages();
 
+        using (var scope = app.Services.CreateScope())
+        {
+            var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var roles = new[] { "Admin", "Manager", "User" };
+            foreach (var role in roles)
+            {
+                if (!await roleManager.RoleExistsAsync(role))
+                {
+                    await roleManager.CreateAsync(new IdentityRole(role));
+                }
+            }
+        }
+        using (var scope = app.Services.CreateScope())
+        {
+            var UserManager = scope.ServiceProvider.GetRequiredService<UserManager<User>>();
+            string email = "admin@admin.com";
+            string password = "Admin1111.";
+            if (await UserManager.FindByEmailAsync(email) == null)
+            {
+                var user = new User();
+                user.UserName = email;
+                user.Email = email;
+                await UserManager.CreateAsync(user, password);
+                await UserManager.AddToRoleAsync(user, "Admin");
+            }
+        }
         app.Run();
     }
 
-    private static async Task CreateRolesAndAdminUser(IServiceProvider serviceProvider)
-    {
-        var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
-        var userManager = serviceProvider.GetRequiredService<UserManager<User>>();
-
-        string[] roles = { "Admin", "Manager", "User" };
-        foreach (var role in roles)
-        {
-            if (!await roleManager.RoleExistsAsync(role))
-            {
-                await roleManager.CreateAsync(new IdentityRole(role));
-            }
-        }
-
-        string email = "admin@admin.com";
-        string password = "Admin1111.";
-        if (await userManager.FindByEmailAsync(email) == null)
-        {
-            var user = new User { UserName = email, Email = email };
-            var result = await userManager.CreateAsync(user, password);
-            if (result.Succeeded)
-            {
-                await userManager.AddToRoleAsync(user, "Admin");
-            }
-        }
-    }
 }
